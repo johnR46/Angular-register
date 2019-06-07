6d025132943afd6fcaf79bedaffa55317a81f008
JohnR46 <Machine.The@hotmail.com>
2019-06-08 01:58:30 +0700

    add routing module and path url

---
+++ Angular7MemberWorkshop/src/app/app.routing.ts
@@ -0,0 +1,12 @@
+import { Routes, RouterModule } from '@angular/router';
+import { AppURL } from './app.url';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+
+const RouteList: Routes = [
+    { path: AppURL.Login, component: LoginComponent },
+    { path: AppURL.Register, component: RegisterComponent },
+
+];
+
+export const AppRouting = RouterModule.forRoot(RouteList);
