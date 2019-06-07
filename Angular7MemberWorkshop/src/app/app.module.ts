87c70be39809b2ac13315ba82c8eca5df6076002
JohnR46 <Machine.The@hotmail.com>
2019-06-08 01:59:38 +0700

    add module and add router-outlet

--- Angular7MemberWorkshop/src/app/app.module.ts
+++ Angular7MemberWorkshop/src/app/app.module.ts
@@ -2,12 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BsDropdownModule } from 'ngx-bootstrap';
 import { AppComponent } from './app.component';
+import { AppRouting } from './app.routing';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
+    AppRouting,
     BrowserModule,
     BsDropdownModule.forRoot()
 
