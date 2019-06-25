c81edd7970cdc382a7427d124733070e4e613a02
JohnR46 <Machine.The@hotmail.com>
2019-06-25 22:41:23 +0700

    change to ng-content

--- Angular7MemberWorkshop/src/app/authentication/shareds/shareds.module.ts
+++ Angular7MemberWorkshop/src/app/authentication/shareds/shareds.module.ts
@@ -3,12 +3,17 @@ import { CommonModule } from '@angular/common';
 import { BsDropdownModule } from 'ngx-bootstrap';
 import { AuthNavbarComponent } from './components/auth-navbar/auth-navbar.component';
 import { AuthSidebarComponent } from './components/auth-sidebar/auth-sidebar.component';
+import { AuthContentComponent } from './components/auth-content/auth-content.component';
 @NgModule({
   imports: [
     CommonModule,
     BsDropdownModule
   ],
-  declarations: [AuthNavbarComponent, AuthSidebarComponent],
-  exports: [AuthNavbarComponent, BsDropdownModule,AuthSidebarComponent],
+  declarations: [
+    AuthNavbarComponent,
+     AuthSidebarComponent,
+      AuthContentComponent
+    ],
+  exports: [AuthNavbarComponent, BsDropdownModule,AuthSidebarComponent,AuthContentComponent],
 })
 export class SharedsModule { }
